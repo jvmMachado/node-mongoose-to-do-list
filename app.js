bd73e1de138b84b563c4d03894013cd453bb3288
jvmMachado <joaovmmachado@gmail.com>
2020-02-17 19:06:01 -0300

    add save-to-db functionalities to custom lists

--- app.js
+++ app.js
@@ -30,59 +30,71 @@ const item2 = new Item({ name: 'Hit the + button to add a new item.' });
 const item3 = new Item({ name: '<-- Hit this to delete an item.' });
 const defaultItems = [item1, item2, item3];
 
-const listSchema = new mongoose.Schema ({
+const listSchema = new mongoose.Schema({
   name: String,
   items: [itemsSchema]
 });
 
 const List = mongoose.model('List', listSchema);
 
-
 app.get('/', function(req, res) {
   let day = date.getDate();
 
   Item.find({}, (err, foundItems) => {
-
     if (err) {
       console.log(err);
-      
     } else if (foundItems.length === 0) {
-
       Item.insertMany(defaultItems, err => {
-
         if (err) {
           console.log(err);
         } else {
           console.log('Default items have been added to the list.');
         }
       });
-        res.redirect('/');
+      res.redirect('/');
     } else {
       res.render('list', { listTitle: day, itemsList: foundItems });
-      }
-    });
+    }
   });
+});
 
 app.post('/', function(req, res) {
-  let postItem = req.body.newItem;
+  const activeList = req.body.list;
+  const postItem = req.body.newItem;
 
-    const newItem = new Item({
-      name: postItem
-    });
+  const newItem = new Item({
+    name: postItem
+  });
 
-    newItem.save(err =>{
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('A new item has been added to the list.');
-      }
-    });
-    res.redirect('/');
+  List.findOne({ name: activeList }, (err, foundList) => {
+    if (err) {
+      console.log(err);
+    } else if (foundList) {
+      foundList.items.push(newItem);
+      foundList.save(err => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`A new item has been added to ${foundList.name} list`);
+          res.redirect(`/${activeList}`);
+        }
+      });
+    } else {
+      newItem.save(err => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('A new item has been added to the root list.');
+          res.redirect('/');
+        }
+      });
+    }
+  });
 });
 
 app.post('/delete', (req, res) => {
   const itemToDelete = req.body.checkbox;
-  Item.findByIdAndRemove(itemToDelete, err =>{
+  Item.findByIdAndRemove(itemToDelete, err => {
     if (err) {
       console.log(err);
     } else {
@@ -95,25 +107,21 @@ app.post('/delete', (req, res) => {
 app.get('/:listName', (req, res) => {
   const listTitle = req.params.listName;
 
-  List.findOne({name: listTitle}, (err, foundList) => {
+  List.findOne({ name: listTitle }, (err, foundList) => {
     if (err) {
       console.log(err);
     } else if (foundList) {
-
       res.render('list', { listTitle: listTitle, itemsList: foundList.items });
-      
     } else {
-
-      const list = new List ({
+      const list = new List({
         name: listTitle,
         items: defaultItems
       });
-    
+
       list.save();
       res.redirect(`/${listTitle}`);
     }
   });
-  
 });
 
 app.listen(3000, function() {
