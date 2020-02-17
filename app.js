7c50fe23c1d5b0036afd9cff2e0b79911ac63301
jvmMachado <joaovmmachado@gmail.com>
2020-02-17 11:24:05 -0300

    add mongoose and insert default items into mongodb

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js');
 
 const app = express();
-const items = ['Buy Food', 'Cook Food', 'Eat Food'];
-const workItems = [];
+// const workItems = [];
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const itemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please, enter a name for your item.']
+  }
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({ name: 'Welcome to your todolist!' });
+const item2 = new Item({ name: 'Hit the + button to add a new item.' });
+const item3 = new Item({ name: '<-- Hit this to delete an item.' });
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Default items have been added to the list.');
+  }
+});
 
 app.get('/', function(req, res) {
   let day = date.getDate();
